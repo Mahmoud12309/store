fae83080d829a8733572b9bf1e12785d88d4d845
Mahmoud12309 <122573299+Mahmoud12309@users.noreply.github.com>
2023-09-05 01:39:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 let shop = document.querySelectorAll(".butons");
 shop.forEach((s) => {
     s.onclick = () => {
-        // احصل على البطاقة المرتبطة بالزر الذي تم النقر عليه
-        let card = s.closest(".card");
-        if (card) {
-            let img = card.querySelector("img");
+        let card = document.querySelectorAll(".card");
+        card.forEach((c) => {
+            let img = c.querySelector("img");
             window.location.href = `https://wa.me/+201010252321/?text=${img.getAttribute("data-name")}`;
-        }
+        });
     };
 });
