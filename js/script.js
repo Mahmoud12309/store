9bd3e27d8d154391d85aa7ba3d997196ad57aea4
Mahmoud12309 <122573299+Mahmoud12309@users.noreply.github.com>
2023-09-05 00:34:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let shop = document.querySelectorAll(".buton");
 shop.forEach((s) => {
     s.onclick = () => {
-        window.location.href = "https://api.whatsapp.com/send?phone=01010252321";
+        window.location.href = "https://api.whatsapp.com/send?phone=+201010252321";
     };
 });
