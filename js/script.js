ca19c5d554b86cf3cf4dfcbd35f05b5a59e620f6
Mahmoud12309 <122573299+Mahmoud12309@users.noreply.github.com>
2023-09-06 02:04:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-let shop = document.querySelectorAll(".butons");
+let shop = document.querySelectorAll(".butons"),
+input = document.querySelector(".dis"),
+num = 100,
+contant = document.querySelector(".contant"),
+contanp = document.querySelector(".contant p"),
+x = document.querySelector(".X"),
+sha = document.querySelector(".sha");
 
 shop.forEach((s, index) => {
     s.onclick = () => {
@@ -7,3 +13,28 @@ shop.forEach((s, index) => {
         window.location.href = `https://wa.me/+201010252321/?text=${img.getAttribute("data-name")}`;
     };
 });
+
+function setTime() {
+    contant.classList.add("active");
+    let set = setInterval(() => {
+        num--;
+        sha.style.width = num + "%";
+        if (num === 0) {
+            clearInterval(set);
+            num = 100;
+        }
+    }, 30);
+    setTimeout(() => {
+        contant.classList.remove("active");
+    }, 3000);
+}
+
+function clickD(e){
+    if(input.value === "D1OaA"){
+        contanp.textContent = "! تمت العملية بنجاح ";
+        setTime();
+    }else{
+        contanp.textContent = "حدث خطاء اعد المحاولة لاحقا";
+        setTime();
+    }
+}
